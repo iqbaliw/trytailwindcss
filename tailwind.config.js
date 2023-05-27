154186479228c6ff78a7c9d6b353c89d371b90bf
Iqbal Wahyudi <iqbalwahyudi20@gmail.com>
2023-05-28 06:29:26 +0700

    margin and custom margin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./public/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            margin: {
+                13: "3.25rem",
+            },
+        },
     },
     plugins: [],
 };
